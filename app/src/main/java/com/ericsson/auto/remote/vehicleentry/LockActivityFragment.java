6673a38a1adf928eba8b43f7deee9007e10456f4
Kristoffer Gronowski <kristoffer.gronowski@ericsson.com>
2015-06-16 09:44:30 -0700

    Updated UI and test android apk and PI LED test
    for the people that do not want to build

--- app/src/main/java/com/ericsson/auto/remote/vehicleentry/LockActivityFragment.java
+++ app/src/main/java/com/ericsson/auto/remote/vehicleentry/LockActivityFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -18,6 +19,19 @@ public class LockActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_lock, container, false);
+        View view = inflater.inflate(R.layout.fragment_lock, container, false);
+
+        Button lock = (Button) view.findViewById(R.id.lock);
+        Button unlock = (Button) view.findViewById(R.id.unlock);
+        Button start = (Button) view.findViewById(R.id.start);
+        Button stop = (Button) view.findViewById(R.id.stop);
+        Button trunk = (Button) view.findViewById(R.id.trunk);
+        Button panic = (Button) view.findViewById(R.id.panic);
+
+        unlock.setEnabled(true);
+        start.setEnabled(true);
+        trunk.setEnabled(true);
+
+        return view;
     }
 }
