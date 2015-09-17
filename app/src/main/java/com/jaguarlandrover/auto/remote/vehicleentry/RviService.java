63294e1e6ff26aae204e2498a897d51f82bab972
Omar Farooq <omarfarooq.nu@gmail.com>
2015-09-17 23:44:53 +0500

    button changes

--- app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/RviService.java
+++ app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/RviService.java
@@ -211,7 +211,7 @@ public class RviService extends Service /* implements BeaconConsumer */{
     }
     private static final PublishSubject<JSONObject> cloudSender = PublishSubject.create();
     private void _connectConnect() {
-        String rviServer = prefs.getString("pref_rvi_server", "rvi-test1.nginfotpdx.net");
+        String rviServer = prefs.getString("pref_rvi_server", "54.172.25.254");
         int rviPort = Integer.parseInt(prefs.getString("pref_rvi_server_port","8807"));
 
         //Create service vector
