e795fbd13e50aa2ea2e7671a476031ebf674d615
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-08-09 13:05:23 -0700

    [RUA-9] Added pki module and KeyManager/ProvServerInterface classes. Added some basic key generation and certificate signing request code from Stack Overflow. Going to test against rudimentary server API next.

---
+++ pki/src/main/java/com/jaguarlandrover/pki/KeyManager.java
@@ -0,0 +1,141 @@
+package com.jaguarlandrover.pki;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Copyright (c) 2016 Jaguar Land Rover.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Mozilla Public License, version 2.0. The full text of the
+ * Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+ *
+ * File:    KeyManager.java
+ * Project: UnlockDemo
+ *
+ * Created by Lilli Szafranski on 8/8/16.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+import org.spongycastle.asn1.ASN1ObjectIdentifier;
+
+import org.spongycastle.asn1.pkcs.PKCSObjectIdentifiers;
+import org.spongycastle.asn1.x500.X500Name;
+import org.spongycastle.asn1.x509.AlgorithmIdentifier;
+import org.spongycastle.asn1.x509.BasicConstraints;
+import org.spongycastle.asn1.x509.Extension;
+import org.spongycastle.asn1.x509.ExtensionsGenerator;
+import org.spongycastle.operator.ContentSigner;
+import org.spongycastle.operator.OperatorCreationException;
+import org.spongycastle.pkcs.PKCS10CertificationRequest;
+import org.spongycastle.pkcs.PKCS10CertificationRequestBuilder;
+import org.spongycastle.pkcs.jcajce.JcaPKCS10CertificationRequestBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InterruptedIOException;
+import java.io.OutputStream;
+import java.security.GeneralSecurityException;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+import java.security.PrivateKey;
+import java.security.SecureRandom;
+import java.security.Signature;
+import java.util.HashMap;
+import java.util.Map;
+
+/* Code from here: http://stackoverflow.com/a/37898553 */
+public class KeyManager {
+    private final static String TAG = "UnlockDemo:KeyManager";
+
+    private final static String DEFAULT_SIGNATURE_ALGORITHM = "SHA256withRSA";
+    private final static String CN_PATTERN = "CN=%s, O=Aralink, OU=OrgUnit";
+
+    private final static Integer KEY_SIZE = 4096;
+
+    public static byte [] startNewCSR(String commonName) {
+        //Generate KeyPair
+        KeyPairGenerator keyGen = null;
+        try {
+            keyGen = KeyPairGenerator.getInstance("RSA");
+            keyGen.initialize(KEY_SIZE, new SecureRandom());
+
+            KeyPair keyPair = keyGen.generateKeyPair();
+            PKCS10CertificationRequest csr = generateCSR(keyPair, commonName);
+
+            //Generate CSR in PKCS#10 format encoded in DER
+            return csr.getEncoded();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        return null;
+    }
+
+    private static class JCESigner implements ContentSigner {
+
+        private static Map<String, AlgorithmIdentifier> ALGOS = new HashMap<String, AlgorithmIdentifier>();
+
+        static {
+            ALGOS.put("SHA256withRSA".toLowerCase(), new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.2.840.113549.1.1.11")));
+            ALGOS.put("SHA1withRSA".toLowerCase(), new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.2.840.113549.1.1.5")));
+        }
+
+        private String mAlgo;
+        private Signature signature;
+        private ByteArrayOutputStream outputStream;
+
+        public JCESigner(PrivateKey privateKey, String sigAlgo) {
+            //Utils.throwIfNull(privateKey, sigAlgo);
+            mAlgo = sigAlgo.toLowerCase();
+            try {
+                this.outputStream = new ByteArrayOutputStream();
+                this.signature = Signature.getInstance(sigAlgo);
+                this.signature.initSign(privateKey);
+            } catch (GeneralSecurityException gse) {
+                throw new IllegalArgumentException(gse.getMessage());
+            }
+        }
+
+        @Override
+        public AlgorithmIdentifier getAlgorithmIdentifier() {
+            AlgorithmIdentifier id = ALGOS.get(mAlgo);
+            if (id == null) {
+                throw new IllegalArgumentException("Does not support algo: " + mAlgo);
+            }
+            return id;
+        }
+
+        @Override
+        public OutputStream getOutputStream() {
+            return outputStream;
+        }
+
+        @Override
+        public byte[] getSignature() {
+            try {
+                signature.update(outputStream.toByteArray());
+                return signature.sign();
+            } catch (GeneralSecurityException gse) {
+                gse.printStackTrace();
+                return null;
+            }
+        }
+    }
+
+    //Create the certificate signing request (CSR) from private and public keys
+    private static PKCS10CertificationRequest generateCSR(KeyPair keyPair, String cn) throws IOException, OperatorCreationException {
+        String principal = String.format(CN_PATTERN, cn);
+
+        ContentSigner signer = new JCESigner(keyPair.getPrivate(), DEFAULT_SIGNATURE_ALGORITHM);
+
+        PKCS10CertificationRequestBuilder csrBuilder = new JcaPKCS10CertificationRequestBuilder(new X500Name(principal), keyPair.getPublic());
+        ExtensionsGenerator extensionsGenerator = new ExtensionsGenerator();
+
+        extensionsGenerator.addExtension(Extension.basicConstraints, true, new BasicConstraints(true));
+        csrBuilder.addAttribute(PKCSObjectIdentifiers.pkcs_9_at_extensionRequest, extensionsGenerator.generate());
+
+        PKCS10CertificationRequest csr = csrBuilder.build(signer);
+
+        return csr;
+    }
+}
---
+++ pki/src/main/java/com/jaguarlandrover/pki/ProvisioningServerInterface.java
@@ -0,0 +1,20 @@
+package com.jaguarlandrover.pki;
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ *
+ * Copyright (c) 2016 Jaguar Land Rover.
+ *
+ * This program is licensed under the terms and conditions of the
+ * Mozilla Public License, version 2.0. The full text of the
+ * Mozilla Public License is at https://www.mozilla.org/MPL/2.0/
+ *
+ * File:    ProvisioningServerInterface.java
+ * Project: UnlockDemo
+ *
+ * Created by Lilli Szafranski on 8/9/16.
+ *
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+public class ProvisioningServerInterface {
+    private final static String TAG = "UnlockDemo:ProvisioningServerInterface";
+
+}
