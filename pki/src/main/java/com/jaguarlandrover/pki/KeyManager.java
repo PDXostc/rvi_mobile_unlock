50be833d71b4b5de37f0fe95b0c5e09569270d35
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-08-17 16:15:15 -0700

    [RUA-9] Added email and commented out unused code

--- pki/src/main/java/com/jaguarlandrover/pki/KeyManager.java
+++ pki/src/main/java/com/jaguarlandrover/pki/KeyManager.java
@@ -59,17 +59,17 @@ import javax.security.auth.x500.X500Principal;
 public class KeyManager {
     private final static String TAG = "UnlockDemo:KeyManager";
 
-    private final static String KEYSTORE_ALIAS = "RVI_KEYPAIR_4096_2";
+    private final static String KEYSTORE_ALIAS = "RVI_KEYPAIR_4096_6";
     private final static String DEFAULT_SIGNATURE_ALGORITHM = "SHA256withRSA";
-    private final static String CN_PATTERN = "CN=%s, O=Aralink, OU=OrgUnit";
+    private final static String CN_PATTERN = "CN=%s, O=Genivi, OU=OrgUnit, EMAILADDRESS=pdxostc.android@gmail.com";
 
     private final static Integer KEY_SIZE = 4096;
 
     static byte [] getCSR(Context context, String commonName) {
 
-        String   principal = String.format(CN_PATTERN, commonName);
-        KeyStore keyStore  = null;
-        KeyPair  keyPair   = null;
+        String principal = String.format(CN_PATTERN, commonName);
+        KeyStore keyStore = null;
+        KeyPair keyPair = null;
 
         java.security.cert.Certificate cert = null;
 
@@ -112,177 +112,7 @@ public class KeyManager {
             e.printStackTrace();
         }
 
-
-
-
-//        try {
-////            keyGen = KeyPairGenerator.getInstance("RSA");
-////            keyGen.initialize(KEY_SIZE, new SecureRandom());
-//
-//            Log.d(TAG, "Generating key pair...");
-//
-////            KeyPair keyPair = keyGen.generateKeyPair();
-//
-//            KeyPair keyPair = getKeyPair(context, principal);
-//
-//            Log.d(TAG, "Key pair generated: " + keyPair.getPrivate().toString() + "/" + keyPair.getPublic().toString());
-//
-//            Log.d(TAG, "Generating CSR...");
-//
-//            PKCS10CertificationRequest csr = generateCSR(keyPair, principal);
-//
-//            Log.d(TAG, "CSR generated: " + csr.toString());
-//
-//            //Generate CSR in PKCS#10 format encoded in DER
-//
-//            Log.d(TAG, "Encoding CSR...");
-//
-//            return csr.getEncoded();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        return null;
-    }
-//
-//        //Generate KeyPair
-//        KeyPairGenerator keyGen = null;
-//        String principal = String.format(CN_PATTERN, commonName);
-//
-//        try {
-////            keyGen = KeyPairGenerator.getInstance("RSA");
-////            keyGen.initialize(KEY_SIZE, new SecureRandom());
-//
-//            Log.d(TAG, "Generating key pair...");
-//
-////            KeyPair keyPair = keyGen.generateKeyPair();
-//
-//            KeyPair keyPair = getKeyPair(context, principal);
-//
-//            Log.d(TAG, "Key pair generated: " + keyPair.getPrivate().toString() + "/" + keyPair.getPublic().toString());
-//
-//            Log.d(TAG, "Generating CSR...");
-//
-//            PKCS10CertificationRequest csr = generateCSR(keyPair, principal);
-//
-//            Log.d(TAG, "CSR generated: " + csr.toString());
-//
-//            //Generate CSR in PKCS#10 format encoded in DER
-//
-//            Log.d(TAG, "Encoding CSR...");
-//
-//            return csr.getEncoded();
-//
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        return null;
-//    }
-
-    private static KeyPair getKeyPair(Context context, String principal) {
-        KeyStore keyStore = null;
-        try {
-            keyStore = KeyStore.getInstance("AndroidKeyStore");
-            keyStore.load(null);
-
-            if (!keyStore.containsAlias(KEYSTORE_ALIAS)) {
-                Calendar start = Calendar.getInstance();
-                Calendar end = Calendar.getInstance();
-                end.add(Calendar.YEAR, 1);
-
-                KeyPairGeneratorSpec spec = new KeyPairGeneratorSpec.Builder(context)
-                        .setAlias(KEYSTORE_ALIAS)
-                        .setKeySize(KEY_SIZE)
-                        .setSubject(new X500Principal(principal))
-                        .setSerialNumber(BigInteger.ONE)
-                        .setStartDate(start.getTime())
-                        .setEndDate(end.getTime())
-                        .build();
-                KeyPairGenerator generator = KeyPairGenerator.getInstance("RSA", "AndroidKeyStore");
-                generator.initialize(spec);
-
-                return generator.generateKeyPair();
-            } else {
-                final Key key = (PrivateKey) keyStore.getKey(KEYSTORE_ALIAS, null);
-
-                final java.security.cert.Certificate cert = keyStore.getCertificate(KEYSTORE_ALIAS);
-                final PublicKey publicKey = cert.getPublicKey();
-
-                return new KeyPair(publicKey, (PrivateKey) key);
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
         return null;
     }
 
-    private static class JCESigner implements ContentSigner {
-
-        private static Map<String, AlgorithmIdentifier> ALGOS = new HashMap<String, AlgorithmIdentifier>();
-
-        static {
-            ALGOS.put("SHA256withRSA".toLowerCase(), new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.2.840.113549.1.1.11")));
-            ALGOS.put("SHA1withRSA".toLowerCase(), new AlgorithmIdentifier(new ASN1ObjectIdentifier("1.2.840.113549.1.1.5")));
-        }
-
-        private String mAlgo;
-        private Signature signature;
-        private ByteArrayOutputStream outputStream;
-
-        public JCESigner(PrivateKey privateKey, String sigAlgo) {
-            //Utils.throwIfNull(privateKey, sigAlgo);
-            mAlgo = sigAlgo.toLowerCase();
-            try {
-                this.outputStream = new ByteArrayOutputStream();
-                this.signature = Signature.getInstance(sigAlgo);
-                this.signature.initSign(privateKey);
-            } catch (GeneralSecurityException gse) {
-                throw new IllegalArgumentException(gse.getMessage());
-            }
-        }
-
-        @Override
-        public AlgorithmIdentifier getAlgorithmIdentifier() {
-            AlgorithmIdentifier id = ALGOS.get(mAlgo);
-            if (id == null) {
-                throw new IllegalArgumentException("Does not support algo: " + mAlgo);
-            }
-            return id;
-        }
-
-        @Override
-        public OutputStream getOutputStream() {
-            return outputStream;
-        }
-
-        @Override
-        public byte[] getSignature() {
-            try {
-                signature.update(outputStream.toByteArray());
-                return signature.sign();
-            } catch (GeneralSecurityException gse) {
-                gse.printStackTrace();
-                return null;
-            }
-        }
-    }
-
-    //Create the certificate signing request (CSR) from private and public keys
-    private static PKCS10CertificationRequest generateCSR(KeyPair keyPair, String principal) throws IOException, OperatorCreationException {
-        ContentSigner signer = new JCESigner(keyPair.getPrivate(), DEFAULT_SIGNATURE_ALGORITHM);
-
-        PKCS10CertificationRequestBuilder csrBuilder = new JcaPKCS10CertificationRequestBuilder(new X500Name(principal), keyPair.getPublic());
-        ExtensionsGenerator extensionsGenerator = new ExtensionsGenerator();
-
-        extensionsGenerator.addExtension(Extension.basicConstraints, true, new BasicConstraints(true));
-        csrBuilder.addAttribute(PKCSObjectIdentifiers.pkcs_9_at_extensionRequest, extensionsGenerator.generate());
-
-        PKCS10CertificationRequest csr = csrBuilder.build(signer);
-
-        return csr;
-    }
 }
