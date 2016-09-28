e3dd49e7e8b6093654026b287a6ea94713b8afaf
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-09-28 11:34:29 -0700

    [RUA-29] Catching json parse exceptions of dlink packets and added callbacks to interfaces for handling packet receive errors

--- rvi/src/main/java/com/jaguarlandrover/rvi/RemoteConnectionManager.java
+++ rvi/src/main/java/com/jaguarlandrover/rvi/RemoteConnectionManager.java
@@ -49,6 +49,11 @@ public class RemoteConnectionManager
             public void onPacketParsed(DlinkPacket packet) {
                 if (mListener != null) mListener.onRVIDidReceivePacket(packet);
             }
+
+            @Override
+            public void onPacketFailedToParse(Throwable error) {
+                if (mListener != null) mListener.onRVIDidFailToReceivePacket(error);
+            }
         });
 
         RemoteConnectionInterface.RemoteConnectionListener connectionListener = new RemoteConnectionInterface.RemoteConnectionListener()
