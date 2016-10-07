6cb8a72af2912e6076075335a5b29576f6afdddf
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-10-06 19:27:32 -0700

    [RCA-23] Removed a bunch of unused and commented-out code

--- app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/ServerNode.java
+++ app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/ServerNode.java
@@ -16,7 +16,6 @@ package com.jaguarlandrover.auto.remote.vehicleentry;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
 import android.os.Handler;
 import android.preference.PreferenceManager;
 import android.util.Base64;
@@ -28,15 +27,8 @@ import com.jaguarlandrover.pki.PKIManager;
 import com.jaguarlandrover.rvi.RVILocalNode;
 import com.jaguarlandrover.rvi.RVIRemoteNode;
 import com.jaguarlandrover.rvi.RVIRemoteNodeListener;
-import com.jaguarlandrover.rvi.ServiceBundle;
 
-import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Type;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -48,7 +40,6 @@ class ServerNode
     private static Context applicationContext = UnlockApplication.getContext();
 
     private static SharedPreferences        preferences = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-    //private static SharedPreferences.Editor editor      = null;//preferences.edit();
     private static Gson                     gson        = new Gson();
 
     private static RVIRemoteNode rviNode = new RVIRemoteNode(null);
@@ -66,7 +57,6 @@ class ServerNode
     private final static String INVOKED_SERVICE_REPORT_KEY      = "INVOKED_SERVICE_REPORT_KEY";
 
     /* RVI fully-qualified service identifier parts */
-//    private final static String RVI_DOMAIN                   = "genivi.org";
     private final static String CREDENTIAL_MANAGEMENT_BUNDLE = "credential_management";
     private final static String REPORTING_BUNDLE             = "report";
 
@@ -104,10 +94,6 @@ class ServerNode
             new ArrayList<>(Arrays.asList(
                     REPORTING_BUNDLE + "/" + SERVICE_INVOKED_BY_GUEST));
 
-//    /* Service bundles */
-//    private final static ServiceBundle credentialManagementServiceBundle = new ServiceBundle(applicationContext, RVI_DOMAIN, CREDENTIAL_MANAGEMENT_BUNDLE, credentialManagementServiceIdentifiers);
-//    private final static ServiceBundle reportingServiceBundle            = new ServiceBundle(applicationContext, RVI_DOMAIN, REPORTING_BUNDLE, reportingServiceIdentifiers);
-
     private enum ConnectionStatus
     {
         DISCONNECTED,
@@ -119,73 +105,7 @@ class ServerNode
 
     private static ServerNode ourInstance = new ServerNode();
 
-    //public static ServerNode getInstance() {
-    //    return ourInstance;
-    //}
-
     private ServerNode() {
-//        applicationContext = UnlockApplication.getContext();
-//        preferences        = PreferenceManager.getDefaultSharedPreferences(applicationContext);
-
-//        /* Listeners */
-//        ServiceBundle.ServiceBundleListener serviceBundleListener = new ServiceBundle.ServiceBundleListener()
-//        {
-//            @Override
-//            public void onServiceInvoked(ServiceBundle serviceBundle, String serviceIdentifier, Object parameters) {
-//                if (serviceBundle.getBundleIdentifier().equals(CREDENTIAL_MANAGEMENT_BUNDLE)) {
-//                    switch (serviceIdentifier) {
-//                        case UPDATE_CREDENTIALS:
-//
-//                            // TODO: Check this
-//                            ArrayList<String> credentials = (ArrayList<String>) ((LinkedTreeMap<String, Object>) parameters).get("credentials");
-//
-//                            RVILocalNode.setCredentials(applicationContext, credentials);
-//
-//                            break;
-//
-//                        case REVOKE_CREDENTIALS:
-//
-//                            RVILocalNode.setCredentials(applicationContext, null);
-//
-//                            break;
-//
-//                        case CERT_RESPONSE:
-//                            //Type collectionType = new TypeToken<Collection<UserCredentials>>() {}.getType();
-//                            //Collection<UserCredentials> remoteCredentials =
-//                            //        gson.fromJson(gson.toJson(gson.fromJson((String) parameters, HashMap.class).get("certificates")), collectionType);
-//
-//                            ServerNode.setRemoteCredentialsList(gson.toJson(gson.fromJson((String) parameters, HashMap.class).get("certificates")));
-//
-//                            break;
-//
-//                        case CERT_PROVISION:
-//                            String certId = ((HashMap<String, String>) parameters).get("certid");
-//                            String certificateJwt = ((HashMap<String, String>) parameters).get("certificate");
-//
-//                            // TODO: Need to handle certs? Probably certs (potentially renamed 'credentials') should be stored and handled in lower-level RVI SDK
-//                            certs.put(certId, certificateJwt);
-//
-//                            String[] token = parseAndValidateJWT(certificateJwt);
-//
-//                            ServerNode.setCertificate(token[1]);
-//
-//                            // TODO: Set up notification system to notify UI that stuff is coming from the server instead of the current polling mechanism
-//                            //sendNotification(RviService.this, getResources().getString(R.string.not_new_key) + " : " + key.getString("id"), "dialog", "New Key", key.getString("id"));
-//                            break;
-//
-//                        case CERT_ACCOUNT_DETAILS:
-//                            ServerNode.setUserCredentials(gson.toJson(parameters));
-//
-//                            break;
-//                        }
-//
-//                } else if (serviceBundle.getBundleIdentifier().equals(REPORTING_BUNDLE)) {
-//                    if (serviceIdentifier.equals(SERVICE_INVOKED_BY_GUEST)) {
-//                        ServerNode.setInvokedServiceReport(gson.toJson(parameters));
-//                    }
-//                }
-//            }
-//        };
 
         RVIRemoteNodeListener nodeListener = new RVIRemoteNodeListener()
         {
@@ -250,9 +170,6 @@ class ServerNode
                             break;
 
                         case CERT_RESPONSE:
-                            //Type collectionType = new TypeToken<Collection<UserCredentials>>() {}.getType();
-                            //Collection<UserCredentials> remoteCredentials =
-                            //        gson.fromJson(gson.toJson(gson.fromJson((String) parameters, HashMap.class).get("certificates")), collectionType);
 
                             ServerNode.setRemoteCredentialsList(gson.toJson(gson.fromJson((String) parameters, HashMap.class).get("certificates")));
 
@@ -302,36 +219,10 @@ class ServerNode
             }
         };
 
-//        try {
-//            rviNode.setKeyStores(getKeyStore("server-certs", "BKS", "password"), getKeyStore("client.p12", "PKCS12", "password"), "password");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        rviNode.addJWTCredentials("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyaWdodF90b19pbnZva2UiOlsiZ2VuaXZpLm9yZyJdLCJpc3MiOiJqbHIuY29tIiwiZGV2aWNlX2NlcnQiOiJNSUlCOHpDQ0FWd0NBUUV3RFFZSktvWklodmNOQVFFTEJRQXdRakVMTUFrR0ExVUVCaE1DVlZNeER6QU5CZ05WQkFnTUJrOXlaV2R2YmpFUk1BOEdBMVVFQnd3SVVHOXlkR3hoYm1ReER6QU5CZ05WQkFvTUJrZEZUa2xXU1RBZUZ3MHhOVEV4TWpjeU16RTBOVEphRncweE5qRXhNall5TXpFME5USmFNRUl4Q3pBSkJnTlZCQVlUQWxWVE1ROHdEUVlEVlFRSURBWlBjbVZuYjI0eEVUQVBCZ05WQkFjTUNGQnZjblJzWVc1a01ROHdEUVlEVlFRS0RBWkhSVTVKVmtrd2daOHdEUVlKS29aSWh2Y05BUUVCQlFBRGdZMEFNSUdKQW9HQkFKdHZpTThBUklyRnF1UGMwbXlCOUJ1RjlNZGtBLzJTYXRxYlpNV2VUT1VKSEdyakJERUVNTFE3ems4QXlCbWk3UnF1WVlaczY3U3lMaHlsVkdLaDZzSkFsZWN4YkhVd2o3Y1pTUzFibUtNamU2TDYxZ0t3eEJtMk5JRlUxY1ZsMmpKbFRhVTlWWWhNNHhrNTd5ajI4bmtOeFNZV1AxdmJGWDJORFgyaUg3YjVBZ01CQUFFd0RRWUpLb1pJaHZjTkFRRUxCUUFEZ1lFQWhicVZyOUUvME03MjluYzZESStxZ3FzUlNNZm95dkEzQ21uL0VDeGwxeWJHa3V6TzdzQjhmR2pnTVE5enpjYjZxMXVQM3dHalBpb3FNeW1pWVlqVW1DVHZ6ZHZSQlorNlNEanJaZndVdVlleGlLcUk5QVA2WEthSGxBTDE0K3JLKzZITjR1SWtaY0l6UHdTTUhpaDFic1RScHlZNVozQ1VEY0RKa1l0VmJZcz0iLCJ2YWxpZGl0eSI6eyJzdGFydCI6MTQ1MjE5Mjc3Nywic3RvcCI6MTQ4MzcyODc3N30sInJpZ2h0X3RvX3JlZ2lzdGVyIjpbImdlbml2aS5vcmciXSwiY3JlYXRlX3RpbWVzdGFtcCI6MTQ1MjE5Mjc3NywiaWQiOiJpbnNlY3VyZV9jcmVkZW50aWFscyJ9.TBDUJFL1IQ039Lz7SIkcblhz62jO35STJ8OiclL_xlxEE_L_EjnELrDOGvkIh7zhhl8RMHkUJcTFQKF7P6WDJ5rUJejXJlkTRf-aVmHqEhpspRw6xD2u_2A9wmTWLJF94_wsEb7M7xWCXVrbexu_oik85zmuxRQgRE5wrTC7DDQ");
-
-//        credentialManagementServiceBundle.setListener(serviceBundleListener);
-//        reportingServiceBundle.setListener(serviceBundleListener);
-
         rviNode.setListener(nodeListener);
-
-//        rviNode.addBundle(credentialManagementServiceBundle);
-//        rviNode.addBundle(reportingServiceBundle);
-
 
         RVILocalNode.addLocalServices(UnlockApplication.getContext(), reportingServiceIdentifiers);
         RVILocalNode.addLocalServices(UnlockApplication.getContext(), credentialManagementServiceIdentifiers);
-    }
-
-    private static KeyStore getKeyStore(String fileName, String type, String password) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException { // type = "jks"?
-        AssetManager assetManager = applicationContext.getAssets();
-        InputStream fis = assetManager.open(fileName);
-
-        KeyStore ks = KeyStore.getInstance(type);
-        ks.load(fis, password.toCharArray());
-        fis.close();
-
-        return ks;
     }
 
     Handler  timerHandler  = new Handler();
@@ -352,14 +243,6 @@ class ServerNode
     private void stopRepeatingTask() {
         timerHandler.removeCallbacks(timerRunnable);
     }
-
-//    public static void setKeyStoresAndPrivileges(KeyStore serverKeyStore, KeyStore deviceKeyStore, String deviceKeyStorePassword, ArrayList<String> privileges) {
-//        rviNode.setKeyStores(serverKeyStore, deviceKeyStore, deviceKeyStorePassword);
-//
-//        if (privileges != null)
-//            for (String jwt : privileges)
-//                rviNode.addJWTCredentials(jwt);
-//    }
 
     static void connect() {
         Log.d(TAG, "Attempting to connect to RVI provisioning server.");
@@ -386,17 +269,7 @@ class ServerNode
             e.printStackTrace();
         }
 
-        //credentialManagementServiceBundle.invokeService(REQUEST_CREDENTIALS, parameters, 5000);
         rviNode.invokeService(CREDENTIAL_MANAGEMENT_BUNDLE + "/" + REQUEST_CREDENTIALS, parameters, 5000);
-
-//        try {
-//            parameters.put("mobileUUID", RVILocalNode.getLocalNodeIdentifier(applicationContext).substring("android/".length()));
-//            parameters.put("vehicleVIN", ServerNode.getUserCredentials().getVehicleVin());
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//
-//        credentialManagementServiceBundle.invokeService(CERT_REQUESTALL, parameters, 5000);
     }
 
     static void modifyRemoteCredentials(UserCredentials remoteCredentials) {
@@ -404,7 +277,6 @@ class ServerNode
 
         if (connectionStatus == ConnectionStatus.DISCONNECTED) connect();
 
-//        credentialManagementServiceBundle.invokeService(CERT_MODIFY, remoteCredentials, 5000);
         rviNode.invokeService(CREDENTIAL_MANAGEMENT_BUNDLE + "/" + CERT_MODIFY, remoteCredentials, 5000);
     }
 
@@ -413,7 +285,6 @@ class ServerNode
 
         if (connectionStatus == ConnectionStatus.DISCONNECTED) connect();
 
-//        credentialManagementServiceBundle.invokeService(CERT_CREATE, remoteCredentials, 5000);
         rviNode.invokeService(CREDENTIAL_MANAGEMENT_BUNDLE + "/" + CERT_CREATE, remoteCredentials, 5000);
     }
 
--- app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/VehicleNode.java
+++ app/src/main/java/com/jaguarlandrover/auto/remote/vehicleentry/VehicleNode.java
@@ -16,19 +16,11 @@ package com.jaguarlandrover.auto.remote.vehicleentry;
 
 import android.content.Context;
 import android.content.SharedPreferences;
-import android.content.res.AssetManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import com.jaguarlandrover.rvi.RVIRemoteNode;
 import com.jaguarlandrover.rvi.RVIRemoteNodeListener;
-import com.jaguarlandrover.rvi.ServiceBundle;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.cert.CertificateException;
 import java.util.*;
 
 public class VehicleNode
@@ -44,24 +36,20 @@ public class VehicleNode
 
     private static RVIRemoteNode rviNode = new RVIRemoteNode(null);
 
-//    /* RVI fully-qualified service identifier parts */
-//    private final static String RVI_DOMAIN        = "genivi.org";
+    /* RVI fully-qualified service identifier parts */
     private final static String FOB_SIGNAL_BUNDLE = "fob";
 
     /* Remote service identifiers */
-    public final static String FOB_SIGNAL_UNLOCK      = "unlock";
-    public final static String FOB_SIGNAL_LOCK        = "lock";
-//    public final static String FOB_SIGNAL_AUTO_UNLOCK = "auto_unlock";
-//    public final static String FOB_SIGNAL_AUTO_LOCK   = "auto_lock";
-    public final static String FOB_SIGNAL_START       = "start";
-    public final static String FOB_SIGNAL_STOP        = "stop";
-    public final static String FOB_SIGNAL_HORN        = "horn";
-    public final static String FOB_SIGNAL_TRUNK       = "trunk";
-    public final static String FOB_SIGNAL_PANIC       = "panic";
-    public final static String FOB_SIGNAL_LIGHTS      = "lights";
-
-//    /* Service bundles */
-//    private final static ServiceBundle fobSignalServiceBundle = new ServiceBundle(applicationContext, RVI_DOMAIN, FOB_SIGNAL_BUNDLE, null);
+    final static String FOB_SIGNAL_UNLOCK      = "unlock";
+    final static String FOB_SIGNAL_LOCK        = "lock";
+//    final static String FOB_SIGNAL_AUTO_UNLOCK = "auto_unlock";
+//    final static String FOB_SIGNAL_AUTO_LOCK   = "auto_lock";
+    final static String FOB_SIGNAL_START       = "start";
+    final static String FOB_SIGNAL_STOP        = "stop";
+    final static String FOB_SIGNAL_HORN        = "horn";
+    final static String FOB_SIGNAL_TRUNK       = "trunk";
+    final static String FOB_SIGNAL_PANIC       = "panic";
+    final static String FOB_SIGNAL_LIGHTS      = "lights";
 
     private enum ConnectionStatus
     {
@@ -75,21 +63,8 @@ public class VehicleNode
 
     private static VehicleNode ourInstance = new VehicleNode();
 
-    //public static VehicleNode getInstance() {
-    //    return ourInstance;
-    //}
-
     private VehicleNode() {
         Log.d(TAG, "VehicleNode()");
-
-//        /* Listeners */
-//        ServiceBundle.ServiceBundleListener serviceBundleListener = new ServiceBundle.ServiceBundleListener()
-//        {
-//            @Override
-//            public void onServiceInvoked(ServiceBundle serviceBundle, String serviceIdentifier, Object parameters) {
-//
-//            }
-//        };
 
         RVIRemoteNodeListener nodeListener = new RVIRemoteNodeListener()
         {
@@ -147,54 +122,22 @@ public class VehicleNode
             }
         };
 
-
-//        try {
-//            rviNode.setKeyStores(getKeyStore("server-certs", "BKS", "password"), getKeyStore("client.p12", "PKCS12", "password"), "password");
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-
-        //rviNode.addJWTCredentials("eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJyaWdodF90b19pbnZva2UiOlsiZ2VuaXZpLm9yZyJdLCJpc3MiOiJqbHIuY29tIiwiZGV2aWNlX2NlcnQiOiJNSUlCOHpDQ0FWd0NBUUV3RFFZSktvWklodmNOQVFFTEJRQXdRakVMTUFrR0ExVUVCaE1DVlZNeER6QU5CZ05WQkFnTUJrOXlaV2R2YmpFUk1BOEdBMVVFQnd3SVVHOXlkR3hoYm1ReER6QU5CZ05WQkFvTUJrZEZUa2xXU1RBZUZ3MHhOVEV4TWpjeU16RTBOVEphRncweE5qRXhNall5TXpFME5USmFNRUl4Q3pBSkJnTlZCQVlUQWxWVE1ROHdEUVlEVlFRSURBWlBjbVZuYjI0eEVUQVBCZ05WQkFjTUNGQnZjblJzWVc1a01ROHdEUVlEVlFRS0RBWkhSVTVKVmtrd2daOHdEUVlKS29aSWh2Y05BUUVCQlFBRGdZMEFNSUdKQW9HQkFKdHZpTThBUklyRnF1UGMwbXlCOUJ1RjlNZGtBLzJTYXRxYlpNV2VUT1VKSEdyakJERUVNTFE3ems4QXlCbWk3UnF1WVlaczY3U3lMaHlsVkdLaDZzSkFsZWN4YkhVd2o3Y1pTUzFibUtNamU2TDYxZ0t3eEJtMk5JRlUxY1ZsMmpKbFRhVTlWWWhNNHhrNTd5ajI4bmtOeFNZV1AxdmJGWDJORFgyaUg3YjVBZ01CQUFFd0RRWUpLb1pJaHZjTkFRRUxCUUFEZ1lFQWhicVZyOUUvME03MjluYzZESStxZ3FzUlNNZm95dkEzQ21uL0VDeGwxeWJHa3V6TzdzQjhmR2pnTVE5enpjYjZxMXVQM3dHalBpb3FNeW1pWVlqVW1DVHZ6ZHZSQlorNlNEanJaZndVdVlleGlLcUk5QVA2WEthSGxBTDE0K3JLKzZITjR1SWtaY0l6UHdTTUhpaDFic1RScHlZNVozQ1VEY0RKa1l0VmJZcz0iLCJ2YWxpZGl0eSI6eyJzdGFydCI6MTQ1MjE5Mjc3Nywic3RvcCI6MTQ4MzcyODc3N30sInJpZ2h0X3RvX3JlZ2lzdGVyIjpbImdlbml2aS5vcmciXSwiY3JlYXRlX3RpbWVzdGFtcCI6MTQ1MjE5Mjc3NywiaWQiOiJpbnNlY3VyZV9jcmVkZW50aWFscyJ9.TBDUJFL1IQ039Lz7SIkcblhz62jO35STJ8OiclL_xlxEE_L_EjnELrDOGvkIh7zhhl8RMHkUJcTFQKF7P6WDJ5rUJejXJlkTRf-aVmHqEhpspRw6xD2u_2A9wmTWLJF94_wsEb7M7xWCXVrbexu_oik85zmuxRQgRE5wrTC7DDQ");
-
-//        fobSignalServiceBundle.setListener(serviceBundleListener);
-
         rviNode.setListener(nodeListener);
-
-//        rviNode.addBundle(fobSignalServiceBundle);
     }
 
-    private static KeyStore getKeyStore(String fileName, String type, String password) throws IOException, KeyStoreException, CertificateException, NoSuchAlgorithmException { // type = "jks"?
-        AssetManager assetManager = applicationContext.getAssets();
-        InputStream fis = assetManager.open(fileName);
-
-        KeyStore ks = KeyStore.getInstance(type);
-        ks.load(fis, password.toCharArray());
-        fis.close();
-
-        return ks;
-    }
-
-    public static boolean isConnecting() {
+    static boolean isConnecting() {
         return (connectionStatus == ConnectionStatus.CONNECTING);
     }
 
-    public static boolean isConnected() {
+    static boolean isConnected() {
         return (connectionStatus == ConnectionStatus.CONNECTED);
     }
 
-    public static boolean isUnlocked() {
+    static boolean isUnlocked() {
         return isUnlocked;
     }
 
-//    public static void setKeyStoresAndPrivileges(KeyStore serverKeyStore, KeyStore deviceKeyStore, String deviceKeyStorePassword, ArrayList<String> privileges) {
-//        rviNode.setKeyStores(serverKeyStore, deviceKeyStore, deviceKeyStorePassword);
-//
-//        for (String jwt : privileges) {
-//            rviNode.addJWTCredentials(jwt);
-//        }
-//    }
-
-    public static void connect() {
+    static void connect() {
         if (connectionStatus == ConnectionStatus.CONNECTING) return; // TODO: Do we want to move this logic down into the SDK?
 
         Log.d(TAG, "Attempting to connect to vehicle.");
@@ -202,33 +145,26 @@ public class VehicleNode
         rviNode.setServerUrl(preferences.getString("pref_rvi_vehicle_url", "38.129.64.40"));
         rviNode.setServerPort(Integer.parseInt(preferences.getString("pref_rvi_vehicle_port", "8807")));
 
-//        rviNode.setBluetoothServiceRecord(UUID.fromString("00001101-0000-1000-8000-00805F9B34FB".toLowerCase())); /* SerialPortServiceClass */
-//        rviNode.setBluetoothChannel(Integer.parseInt(preferences.getString("pref_bt_channel", "1")));
-
         connectionStatus = ConnectionStatus.CONNECTING;
 
         rviNode.connect();
     }
 
-    public static void disconnect() {
+    static void disconnect() {
         connectionStatus = ConnectionStatus.DISCONNECTING;
 
         rviNode.disconnect();
     }
 
-    public static void sendFobSignal(String fobSignal) {//}, Object params) {
+    static void sendFobSignal(String fobSignal) {
         Log.d(TAG, "Sending signal to car: " + fobSignal);
 
         if (connectionStatus == ConnectionStatus.DISCONNECTED) connect();
 
-        //fobSignalServiceBundle.invokeService(fobSignal, new FobParamsManager.FobParams(), 5000);
         rviNode.invokeService(FOB_SIGNAL_BUNDLE + "/" + fobSignal, new FobParamsManager.FobParams(), 5000);
 
         if (fobSignal.equals(FOB_SIGNAL_LOCK)) isUnlocked = false;
         if (fobSignal.equals(FOB_SIGNAL_UNLOCK)) isUnlocked = true;
     }
 
-//    public static void setDeviceAddress(String deviceAddress) {
-//        rviNode.setBluetoothDeviceAddress(deviceAddress);
-//    }
 }
