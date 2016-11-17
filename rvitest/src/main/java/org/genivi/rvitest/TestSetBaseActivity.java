c49a5fbb725fde091fdd4a7d94db409c4d4be07a
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-11-17 14:46:19 -0800

    [RUA-39] Added a bunch of ui stuff to the rvitest module for doing tests and stuff. Not really tested yet, though.

---
+++ rvitest/src/main/java/org/genivi/rvitest/TestSetBaseActivity.java
@@ -0,0 +1,30 @@
+package org.genivi.rvitest;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class TestSetBaseActivity extends AppCompatActivity {
+        private static final String TAG = "RVITest/TestSetBaseActv";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_set_base);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+
+}
