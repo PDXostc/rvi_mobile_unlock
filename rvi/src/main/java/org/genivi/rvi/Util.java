d3be0c84963c656718aca1cad36768b4c15acd19
Lilli Szafranski (Jaguar - Land Rover) <lszafran@jaguarlandrover.com>
2016-11-30 12:07:08 -0800

    [RCA-44] Updated util FQSI validation methods per the last discussion we all had on what would be allowed in each topic level.

--- rvi/src/main/java/org/genivi/rvi/Util.java
+++ rvi/src/main/java/org/genivi/rvi/Util.java
@@ -61,45 +61,106 @@ class Util
 
     }
 
+    static String rfc1035(String domain) {
+
+        /* Domains can't be null and can't be empty. */
+        if (domain == null) throw new IllegalArgumentException("Domain can't be null.");
+        if (domain.equals("")) throw new IllegalArgumentException("Domain can't be an empty string.");
+
+        // TODO: Validation for reserved services; service_component_parts with '$' or something; maybe check against known list
+
+        /* Whitespace isn't allowed. */
+        String regex = "\\s";
+        boolean hasWhiteSpace = domain.matches(regex);
+
+        if (hasWhiteSpace)
+            throw new IllegalArgumentException("Domain \"" + domain + "\" contains a white-space character. Per the RFC1035 specification, only the following characters are allowed: a-z, A-Z, 0-9, '.', and '-'.");
+
+        /* Only the following characters are allowed: a-z, A-Z, 0-9, '.', and '-'. */
+        regex = "^[a-zA-Z0-9-\\.]+$";
+
+        boolean hasSpecialChar = !domain.matches(regex);
+
+        if (hasSpecialChar)
+            throw new IllegalArgumentException("Domain \"" + domain + "\" contains an illegal character. Per the RFC1035 specification, only the following characters are allowed: a-z, A-Z, 0-9, '.', and '-'.");
+
+        /* Check that the domain does not begin or end with a period */
+        if (domain.startsWith("."))
+            throw new IllegalArgumentException("Domain \"" + domain + "\" cannot begin with a '.' character, per the RFC1035 specification.");
+
+        if (domain.endsWith("."))
+            throw new IllegalArgumentException("Domain \"" + domain + "\" cannot begin with a '.' character, per the RFC1035 specification.");
+
+        /* Check that each label of the domain is at least one character and that the first character is a letter. */
+        String[] domainLabels = domain.split(".");
+
+        for (String domainLabel : domainLabels) {
+            if (domainLabel.equals(""))
+                throw new IllegalArgumentException("Domain \"" + domain + "\" contains an empty label/subdomain.");
+
+            regex = "^[a-zA-Z]";
+
+            boolean startsWithNonLetter = !domainLabel.matches(regex);
+
+            if (startsWithNonLetter)
+                throw new IllegalArgumentException("Domain \"" + domain + "\" contains a label/subdomain that begins with a non-letter: \"" + domainLabel + "\". Per the RFC1035 specification, labels/subdomains must begin with a letter.");
+        }
+
+        return domain;
+    }
+
     // TODO: Test!
-    /* Strings that begin or end with '/' have the '/'s removed. Domain component can have '.'s and not '/'s. All other components can have '/'s and not '.'s. */
-    static String validated(String identifierComponent, boolean isDomain) {
+    /* An identifierComponent can have multiple topics in one string, e.g., "foo/bar/baz", but can't have empty topics, e.g., "foo//bar/baz", or begin
+       or end with a topic separator string, e.g., "/foo/bar/baz/'. They can't be empty or null. They can't contain illegal characters: '+' and '#'.
+       Topics that begin with the '$' are reserved, but we aren't checking that at this point. */
+    static String validated(String identifierComponent) {
 
         /* Components can't be null and can't be empty. */
         if (identifierComponent == null) throw new IllegalArgumentException("Component can't be null.");
         if (identifierComponent.equals("")) throw new IllegalArgumentException("Component can't be an empty string.");
+        
+//        /* Whitespace isn't allowed. */
+//        String regex = "\\s";
+//        boolean hasWhiteSpace = identifierComponent.matches(regex);
+//
+//        if (hasWhiteSpace)
+//            throw new IllegalArgumentException("Component \"" + identifierComponent + "\" contains a white-space character. Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.', and '/'.");
 
-        // TODO: Validation for reserved services; service_component_parts with '$' or something; maybe check against known list
-        // TODO: Validation for domain component: check correctness of '.' usage (starting, multiple, repeating, etc.)
-        // TODO: If we decide to allow '.'s for non-domain components, update
-        // TODO: If we decide that components can't start with '_' or '-', have to split on '/' and check that too
-
-        /* Whitespace isn't allowed. */
-        String regex = "\\s";
-        boolean hasWhiteSpace = identifierComponent.matches(regex);
-
-        if (hasWhiteSpace)
-            throw new IllegalArgumentException("Component \"" + identifierComponent + "\" contains a white-space character. Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.', and '/'.");
-
-        /* Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.' (for domains), and '/' (for non-domains). */
-        if (isDomain)
-            regex = "^[a-zA-Z0-9-_\\.]+$";
-        else
-            regex = "^[a-zA-Z0-9-_/]+$";
-
-        boolean hasSpecialChar = !identifierComponent.matches(regex);
-
-        if (hasSpecialChar)
-            throw new IllegalArgumentException("Component \"" + identifierComponent + "\" contains an illegal character. Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.', and '/'.");
+//        /* Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.' (for domains), and '/' (for non-domains). */
+//        if (isDomain)
+//            regex = "^[a-zA-Z0-9-_\\.]+$";
+//        else
+//            regex = "^[a-zA-Z0-9-_/]+$";
+//
+//        boolean hasSpecialChar = !identifierComponent.matches(regex);
+//
+//        if (hasSpecialChar)
+//            throw new IllegalArgumentException("Component \"" + identifierComponent + "\" contains an illegal character. Only the following characters are allowed: a-z, A-Z, 0-9, '-', '_', '.', and '/'.");
 
         /* Look for repeating '/'s. */
-        regex = "/\1{2,}";
+        String regex = "/\1{2,}";
         boolean hasRepeatingSlash = identifierComponent.matches(regex);
 
         if (hasRepeatingSlash)
             throw new IllegalArgumentException("Component \"" + identifierComponent + "\" contains an illegal character sequence: two or more '/'s in a row.");
 
-        /* Trim leading and trailing '/'s for non-domain components. (If it was a domain, they already would have thrown an exception.) */
-        return identifierComponent.replaceAll("^/", "").replaceAll("/$", "");
+        /* Split the string by the topic separator character, and check each bit to make sure it's legal */
+        /* Check that each label of the domain is at least one character and that the first character is a letter. */
+        String[] topicLevels = identifierComponent.split("/");
+
+        for (String topicLevel : topicLevels) {
+            if (topicLevel.equals("")) /* This should have already been caught above... */
+                throw new IllegalArgumentException("Identifier component \"" + identifierComponent + "\" contains an empty topic level.");
+
+            if (topicLevel.contains("+"))
+                throw new IllegalArgumentException("Identifier component \"" + identifierComponent + "\" contains an illegal character: '+'.");
+
+            if (topicLevel.contains("#"))
+                throw new IllegalArgumentException("Identifier component \"" + identifierComponent + "\" contains an illegal character: '#'.");
+
+            // TODO: Validation for reserved services; service_component_parts with '$' or something; maybe check against known list
+        }
+
+        return identifierComponent;
     }
 }
